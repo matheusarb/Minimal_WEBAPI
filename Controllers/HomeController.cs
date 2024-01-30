2244c37419de6a02434abc2f9222f799742478fd
Matheus <mat.araujoribeiro@gmail.com>
2024-01-30 19:33:35 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Blog.Controllers;
+
+[ApiController]
+[Route("")]
+public class HomeController : ControllerBase
+{
+    //Heatl checker - verifica se a API está online
+    [HttpGet]
+    [Route("")]
+    public IActionResult Get()
+        => Ok(new { message = "API rodando" });
+    
+
+}
