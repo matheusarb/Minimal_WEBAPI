644a0c81594dbc113b1c35a798cf6f4f18d055f0
Matheus <mat.araujoribeiro@gmail.com>
2024-01-30 19:51:49 -0300

    Get()

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers;
+
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    [HttpGet("v1/categories")]
+    public IActionResult Get([FromServices]BlogDataContext context)
+    {
+        var categories = context
+            .Categories
+            .AsNoTracking()
+            .ToList();
+        
+        return Ok(categories);
+    }
+}
