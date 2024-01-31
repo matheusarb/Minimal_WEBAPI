920e315220a1043e2c60b75227220954c6a62f51
Matheus <mat.araujoribeiro@gmail.com>
2024-01-31 17:21:00 -0300

    TokenService

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,24 @@
-﻿namespace Blog.Services;
+﻿using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Blog.Services;
 
 public class TokenService
 {
-    public string GenerateToken { get; set; }
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddHours(2),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
 }
