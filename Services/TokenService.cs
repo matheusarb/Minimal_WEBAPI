c8d0ca8fa8f43aa6b78509dd56c839e38609448f
Matheus <mat.araujoribeiro@gmail.com>
2024-02-01 10:23:51 -0300

    Add Claims

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Models;
+﻿using Blog.Extensions;
+using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,14 +13,11 @@ public class TokenService
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var claims = user.GetClaims();
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new Claim(ClaimTypes.Name, "andrebaltieri"), //User.Identity.Name
-                new Claim(ClaimTypes.Role, "admin") //User.IsInRole
-                
-            }),
+            Subject = new ClaimsIdentity(claims),
+
             Expires = DateTime.UtcNow.AddHours(2),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
