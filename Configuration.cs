f62bfaa1358ac8a3d185bd07678213a25e52cf4b
Matheus <mat.araujoribeiro@gmail.com>
2024-02-02 11:31:24 -0300

    Keys and Smtp

--- Configuration.cs
+++ Configuration.cs
@@ -8,4 +8,16 @@ public static class Configuration
     // API Key
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "curso_api_IlTevUm/Zosdw3NwC/uqw==";
+
+    //Smtp
+    public static SmtpConfiguration Smtp = new();
+
+    //Envio de email
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
 }
