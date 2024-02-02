6373a8f85b6b62e37160491aacd7e70ecf7c8652
Matheus <mat.araujoribeiro@gmail.com>
2024-02-02 15:34:12 -0300

    UploadImageViewModel

--- ViewModels/Categories/EditorCategoryViewModel.cs
+++ ViewModels/Categories/EditorCategoryViewModel.cs
@@ -1,12 +1,12 @@
 ﻿using Blog.Models;
 using System.ComponentModel.DataAnnotations;
 
-namespace Blog.ViewModels;
+namespace Blog.ViewModels.Categories;
 
 public class EditorCategoryViewModel
 {
     [Required(ErrorMessage = "O nome é obrigatório")]
-    [StringLength(40, MinimumLength = 3, ErrorMessage ="Este campo deve conter no mínimo 3 e no máximo 40 caracteres")]
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter no mínimo 3 e no máximo 40 caracteres")]
     public string Name { get; set; }
     [Required(ErrorMessage = "O slug é obrigatório")]
     public string Slug { get; set; }
