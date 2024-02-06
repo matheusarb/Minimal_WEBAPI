cd71e4d4c3a4ac38a49995a9c63f3eba21683d67
Matheus <mat.araujoribeiro@gmail.com>
2024-02-06 14:38:42 -0300

    GetByCategoryAsync

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,4 +1,6 @@
 ﻿using Blog.Data;
+using Blog.Models;
+using Blog.ViewModels;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,32 +11,117 @@ namespace Blog.Controllers;
 public class PostController : ControllerBase
 {
     private readonly BlogDataContext _context;
-    
+
     public PostController(BlogDataContext context)
     {
         _context = context;
     }
 
     [HttpGet("v1/posts")]
-    public async Task<IActionResult> GetAsync()
+    public async Task<IActionResult> GetAsync(
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25)
     {
-        var posts = await _context
-            .Posts
-            .AsNoTracking()
-            .Include(x=>x.Category)
-            .Include(x=>x.Author)
-            //.Select(x => new ListPostsViewModel
-            //{
-            //    Id = x.Id,
-            //    Title = x.Title,
-            //    Slug = x.Slug,
-            //    LastUpdateDate = x.LastUpdateDate,
-            //    Category = x.Category.Name,
-            //    Author = $"{x.Author.Name} ({x.Author.Email})"
-            //})
-            .ToListAsync();
-        return Ok(posts);
+        try
+        {
+            var count = await _context.Posts.AsNoTracking().CountAsync();
+            var posts = await _context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.Id)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<List<Post>>("05X41 - Falha interna no servidor"));
+        }
     }
 
+    [HttpGet("v1/posts/{id:int}")]
+    public async Task<IActionResult> DetailsAsync(
+            [FromRoute] int id)
+    {
+        try
+        {
+            var post = await _context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .ThenInclude(x => x.Roles)
+                .Include(x => x.Category)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (post == null)
+                return NotFound(new ResultViewModel<Post>("Conteúdo não encontrado."));
 
+            return Ok(new ResultViewModel<Post>(post));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<List<Post>>("05X41 - Falha interna no servidor"));
+        }
+    }
+
+    [HttpGet("v1/posts/category/{category}")]
+    public async Task<IActionResult> GetByCategoryAsync(
+        [FromRoute] string category,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25)
+    {
+        try
+        {
+            var count = await _context.Posts.CountAsync();
+            var postsByCategory = await _context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .Where(x => x.Slug == category)
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.Id)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                postsByCategory
+            }));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<List<Post>>("05X42 - Falha interna no servidor"));
+        }
+    }
 }
