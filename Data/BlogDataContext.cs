4b375da6981d0ab49a9b6826aed434d74540b791
Matheus <mat.araujoribeiro@gmail.com>
2024-02-06 13:31:05 -0300

    Db connectionstring change

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -11,7 +11,7 @@ namespace Blog.Data
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
+            => options.UseSqlServer("Server=localhost,1433;Database=BlogII;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
